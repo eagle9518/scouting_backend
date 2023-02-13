8402f2be497540415142d549c72504bc89447f97
eagle9518 <j007wo2@gmail.com>
2023-02-13 01:58:33 -0800

    Basic Template, Views, and Urls

--- scouting_backend/urls.py
+++ scouting_backend/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from teams import views as team_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('teams/', team_views.display_teams, name='teams'),
+    path('teams/<int:team_number>', team_views.team_page, name='team_page'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from teams.models import Teams
+
+
+def display_teams(request):
+    if not Teams.objects.filter(team_number=2073).exists():
+        Teams.objects.create(team_number=2073)
+    all_teams = Teams.objects.all()
+    return render(request, 'view_teams.html', {'all_teams': all_teams})
+
+
+def team_page(request, team_number):
+    team = Teams.objects.get(team_number=team_number)
+    return render(request, 'team_page.html', {'team': team})
